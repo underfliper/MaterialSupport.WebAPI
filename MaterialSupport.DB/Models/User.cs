6da1a82aa8d782a27512a7018ffd66177812ea67
Ruslan Mingaleev <neoch56@mail.ru>
2022-03-19 08:58:46 +0300

    Create User model

---
+++ MaterialSupport.DB/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace MaterialSupport.DB.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public Roles Role { get; set; }
+    }
+
+    public enum Roles
+    {
+        Admin,
+        Moderator,
+        Student
+    }
+}
