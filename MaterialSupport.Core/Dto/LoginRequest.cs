35d7824a3aaca437631877feb0e22d5224899cdc
Ruslan Mingaleev <neoch56@mail.ru>
2022-03-21 17:37:11 +0300

    Create new dto's
    New dto's have been added: AuthenticatedUser and LoginRequest

---
+++ MaterialSupport.Core/Dto/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaterialSupport.Core.Dto
+{
+    public class LoginRequest
+    {
+        [Required(ErrorMessage = "Поле Имя пользователя должно быть заполнено!")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Поле Пароль должно быть заполнено!")]
+        public string Password { get; set; }
+    }
+}
