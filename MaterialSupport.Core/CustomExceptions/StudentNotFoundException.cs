4d1ecadee6a0f3632c2a6b82e4d0302e58270a3d
Ruslan Mingaleev <neoch56@mail.ru>
2022-04-10 20:54:09 +0300

    Create new custom exception

---
+++ MaterialSupport.Core/CustomExceptions/StudentNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MaterialSupport.Core.CustomExceptions
+{
+    public class StudentNotFoundException : Exception
+    {
+        public StudentNotFoundException()
+        {
+        }
+
+        public StudentNotFoundException(string message) : base(message)
+        {
+        }
+
+        public StudentNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected StudentNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
